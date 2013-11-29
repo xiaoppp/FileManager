280787fe59b342cb5d888e1cb50f6d2f6b144b5e
xiaop <xiaoppp@gmail.com>
2013-11-29 21:11:31 +0800

    Init

---
+++ src/FileManager/MainWindow.xaml.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+
+using System.IO;
+using System.Xml.Linq;
+using Microsoft.Win32;
+using System.Windows.Forms;
+
+namespace DatumManage
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.Loaded += new RoutedEventHandler(MainWindow_Loaded);
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void ExpandRecursively(ItemsControl itemsControl, bool expand)
+        {
+            ItemContainerGenerator itemContainerGenerator = itemsControl.ItemContainerGenerator;
+
+            for (int i = itemsControl.Items.Count - 1; i >= 0; --i)
+            {
+                ItemsControl childControl = itemContainerGenerator.ContainerFromIndex(i) as ItemsControl;
+                if (childControl != null)
+                    ExpandRecursively(childControl, expand);
+            }
+
+            TreeViewItem treeViewItem = itemsControl as TreeViewItem;
+
+            if (treeViewItem != null)
+                treeViewItem.IsExpanded = expand;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var list = tvFolder.ItemsSource as List<TreeNodeModel>;
+
+            XDocument document = new XDocument();
+            XElement element = new XElement("Root");
+            BuildXML(list, element);
+            document.Add(element);
+
+            Microsoft.Win32.SaveFileDialog dialog = new Microsoft.Win32.SaveFileDialog();
+            dialog.DefaultExt = "xml";
+            if (dialog.ShowDialog().Value)
+            {
+                var path = dialog.FileName;
+                document.Save(path);
+            }
+        }
+
+        private void BuildXML(List<TreeNodeModel> list, XElement ele)
+        {
+            foreach (TreeNodeModel model in list)
+            {
+                if (model.IsSelected)
+                {
+                    XElement pathElement = new XElement("Path",
+                        new XAttribute("Name", model.Name),
+                        //new XAttribute("IsSelected", model.IsSelected),
+                        new XAttribute("FullPath", model.FullPath));
+                    ele.Add(pathElement);
+
+                    if (model.NodeList.Count == 0)
+                    {
+                        var files = Directory.GetFiles(model.FullPath);
+
+                        if (files != null)
+                        {
+                            foreach (var file in files)
+                            {
+                                var filename = Path.GetFileName(file);
+                                XElement fileElement = new XElement("File",
+                                    new XAttribute("Name", filename),
+                                    new XAttribute("Path", file));
+
+                                pathElement.Add(fileElement);
+                            }
+                        }
+                    }
+
+                    BuildXML(model.NodeList, pathElement);
+                }
+            }
+        }
+
+        private void btnBrowseButton_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                txtPath.Text = dialog.SelectedPath;
+            }
+
+            var path = txtPath.Text;
+
+            if (string.IsNullOrEmpty(path))
+                return;
+
+            var model = new TreeNodeModel();
+            model.IsRoot = true;
+
+            BuildFolderNodes(path, model);
+            this.tvFolder.ItemsSource = model.NodeList;
+
+            ExpandRecursively(tvFolder, true);
+        }
+
+        private void BuildFolderNodes(string path, TreeNodeModel model)
+        {
+            var dirs = Directory.GetDirectories(path);
+            var name = Path.GetFileName(path);
+
+            var childModel = new TreeNodeModel { FullPath = path, Name = name };
+
+            model.NodeList.Add(childModel);
+
+            foreach (string dir in dirs)
+            {
+                BuildFolderNodes(dir, childModel);
+            }
+        }
+    }
+}
+
+public class TreeNodeModel {
+
+    public TreeNodeModel()
+    {
+        NodeList = new List<TreeNodeModel>();
+        IsSelected = true;
+        IsRoot = false;
+    }
+
+    public string Name { get; set; }
+    public bool IsSelected { get; set; }
+    public string FullPath { get; set; }
+
+    public List<TreeNodeModel> NodeList { get; set; }
+
+    public bool IsRoot { get; set; }
+}
---
+++ src/FileManager/data/htmlstyle.xslt
@@ -0,0 +1,81 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+
+<!-- 
+     Author	: Benjamin Peng
+     Version	:1.0
+-->
+
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+  <xsl:output  method="html" indent="yes" doctype-public='-//W3C//DTD XHTML 1.0 Transitional//EN'/>
+
+  <xsl:template match="/">
+    <html>
+      <head>
+        <title>index</title>
+        <link href="style.css" rel="Stylesheet" type="text/css" media="screen" />
+      </head>
+      <body>
+        <div id="wrapper">
+          <div id="header">
+            <div>
+              <a>
+                <xsl:attribute name="href">
+                  <xsl:value-of select="/Root/Path/@FullPath" disable-output-escaping="yes" />
+                </xsl:attribute>
+                <xsl:value-of select="/Root/Path/@Name" disable-output-escaping="yes" /> Index
+              </a>
+              <br />
+            </div>
+          </div>
+
+          <div class="line">
+          </div>
+
+          <!--<xsl:apply-templates select="/Root/Path/Path">
+          </xsl:apply-templates>-->
+
+          <xsl:call-template name="t">
+            <xsl:with-param name="collection" select="/Root/Path/Path"></xsl:with-param>
+          </xsl:call-template>
+
+        </div>
+      </body>
+    </html>
+  </xsl:template>
+
+  <xsl:template name="t">
+    <xsl:param name="collection" />
+    <xsl:for-each select="$collection">
+      <div class="block">
+        <div class="title">
+          <span>
+            <a>
+              <xsl:attribute name="href">
+                <xsl:value-of select="@FullPath" disable-output-escaping="yes" />
+              </xsl:attribute>
+              <xsl:value-of select="@Name" disable-output-escaping="yes" />
+            </a>
+          </span>
+        </div>
+        <div class="content">
+          <xsl:if test="count($collection) &gt; 0">
+            <xsl:call-template name="t">
+              <xsl:with-param name="collection" select="Path"></xsl:with-param>
+            </xsl:call-template>
+          </xsl:if>
+          <ul>
+            <xsl:for-each select="File">
+              <li style="margin-left:20px">
+                <xsl:attribute name="href">
+                  <xsl:value-of select="@FullPath" disable-output-escaping="yes" />
+                </xsl:attribute>
+                <xsl:value-of select="@Name" disable-output-escaping="yes" />
+              </li>
+            </xsl:for-each>
+          </ul>
+        </div>
+      </div>
+      <div class="clear"></div>
+    </xsl:for-each>
+  </xsl:template>
+</xsl:stylesheet>
